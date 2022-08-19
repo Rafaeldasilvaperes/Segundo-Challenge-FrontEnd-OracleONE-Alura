07318a6079618bfca85affcc261e2ba05f43db13
unknown <rafaeldasilvaperes@hotmail.com>
2022-08-18 22:02:30 -0300

    Consertando css barra de procurar por char e organizando função de procurar por char

--- scripts/search-btn/filtros/filtro-geral-por-char.js
+++ scripts/search-btn/filtros/filtro-geral-por-char.js
@@ -1,24 +1,30 @@
 export function filtroPorChar(){
-const inputPesquisar = document.querySelector('[data-header-search-bar]')
+  const inputPesquisar = document.querySelector('[data-header-search-bar]')
+  const lupaPesquisar = document.querySelector('[data-header-search-btn]')
 
-inputPesquisar.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      let produtos = document.querySelectorAll('[data-produto]')
-      if(this.value.length > 0){
-        produtos.forEach(produto =>{
-          let expressao = new RegExp(this.value, "i")
-          let produtoTitulo = produto.querySelector('[data-name]').textContent
-          if(expressao.test(produtoTitulo)){
-            produto.closest('li').classList.remove('produto__invisivel')
-          }else{
-            produto.closest('li').classList.add('produto__invisivel')
-          }
-        })
-      }else{
-        produtos.forEach(produto =>{
-          produto.closest('li').classList.remove('produto__invisivel')
-        })
-      }
+  lupaPesquisar.addEventListener('click', aposEnterOuClick);
+  inputPesquisar.addEventListener('keypress', function(e){
+    if (e.key === 'Enter' || e.key === 13){
+      aposEnterOuClick();
     }
   });
+
+  function aposEnterOuClick(){
+    let produtos = document.querySelectorAll('[data-produto]')
+    if(inputPesquisar.value.length > 0){
+      produtos.forEach(produto =>{
+        let expressao = new RegExp(inputPesquisar.value, "i")
+        let produtoTitulo = produto.querySelector('[data-name]').textContent
+        if(expressao.test(produtoTitulo)){
+          produto.closest('li').classList.remove('produto__invisivel')
+        }else{
+          produto.closest('li').classList.add('produto__invisivel')
+        }
+      })
+    }else{
+      produtos.forEach(produto =>{
+        produto.closest('li').classList.remove('produto__invisivel')
+      })
+    } 
+  }
 }
